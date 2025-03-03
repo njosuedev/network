5d0ec5dfd088bc7dce2db7922c36c80117dc8397
njosuedev <njosueofficial@gmail.com>
2025-03-03 14:01:33 +0200

    commernce

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,8 @@
-console.log("hello typescript")
+import express from "express"
+import http from "http"
+import cors from "cors"
+import bodyParser from "body-parser"
+import cookieParser from "cookie-parser"
+
+
+// instance
